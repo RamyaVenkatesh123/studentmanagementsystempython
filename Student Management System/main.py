b4997c8321c46dcb6d72c772648ec52dfb41256d
RamyaVenkatesh123 <ramyaindiagreen8608569361@gmail.com>
2023-10-02 18:38:31 +0530

    Files commited through git

---
+++ Student Management System/main.py
@@ -0,0 +1,54 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+from PIL import ImageTk
+
+def login():
+    if usernameEntry.get()=='' or passwordEntry.get()=='':
+        messagebox.showerror('Error','Fields cannot be empty')
+    elif usernameEntry.get()=='ramya' and passwordEntry.get()=='1234':
+        messagebox.showinfo('Success','Welcome')
+        
+    else:
+        messagebox.showerror('Error','Please enter correct credentials')
+
+
+
+window=tk.Tk()
+window.geometry('1280x700+0+0')
+window.resizable(False,False)
+backgroundImage=ImageTk.PhotoImage(file='C:\\Users\\ramya\\Lib\\site-packages\\Student Management System-20230825T162504Z-001\\Student Management System\\bg.jpg')
+bgLabel=Label(window,image=backgroundImage)
+bgLabel.place(x=0,y=0)
+
+loginFrame=Frame(window,bg='white')
+loginFrame.place(x=400,y=150)
+
+logoImage=PhotoImage(file='C:\\Users\\ramya\\Lib\\site-packages\\Student Management System-20230825T162504Z-001\\Student Management System\\logo.png')
+
+logoLabel=Label(loginFrame,image=logoImage)
+logoLabel.grid(row=0,column=0,columnspan=2,pady=10)
+usernameImage=PhotoImage(file='C:\\Users\\ramya\\Lib\\site-packages\\Student Management System-20230825T162504Z-001\\Student Management System\\user.png')
+usernameLabel=Label(loginFrame,image=usernameImage,text='Username',compound=LEFT
+                    ,font=('times new roman',20,'bold'),bg='white')
+usernameLabel.grid(row=1,column=0,pady=10,padx=20)
+
+usernameEntry=Entry(loginFrame,font=('times new roman',20,'bold'),bd=5,fg='royalblue')
+usernameEntry.grid(row=1,column=1,pady=10,padx=20)
+
+passwordImage=PhotoImage(file='C:\\Users\\ramya\\Lib\\site-packages\\Student Management System-20230825T162504Z-001\\Student Management System\\password.png')
+passwordLabel=Label(loginFrame,image=passwordImage,text='Password',compound=LEFT
+                    ,font=('times new roman',20,'bold'),bg='white')
+passwordLabel.grid(row=2,column=0,pady=10,padx=20)
+
+passwordEntry=Entry(loginFrame,font=('times new roman',20,'bold'),bd=5,fg='royalblue')
+passwordEntry.grid(row=2,column=1,pady=10,padx=20)
+
+loginButton=Button(loginFrame,text='Login',font=('times new roman',14,'bold'),width=15
+                   ,fg='white',bg='cornflowerblue',activebackground='cornflowerblue',
+                   activeforeground='white',cursor='hand2',command=login)
+loginButton.grid(row=3,column=1,pady=10)
+
+
+
+window.mainloop()
